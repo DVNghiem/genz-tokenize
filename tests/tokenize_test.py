fa1f0c2309b2fa7c05ad131702c350a7b1219cff
nghiemIUH <vannghiem848@gmail.com>
2022-03-17 09:41:56 +0700

    update version 1.1.3 fix bug vocab

--- tests/tokenize_test.py
+++ tests/tokenize_test.py
@@ -2,6 +2,8 @@ from genz_tokenize import Tokenize, TokenizeForBert, get_embedding_matrix
 TokenizeForBert()
 tokenize = Tokenize()
 
-print(tokenize(['sinh_viên #'], maxlen=10))
-print(tokenize.decode([1, 288, 2]))
-print(get_embedding_matrix().shape)
+print(tokenize.encode('ông'))
+print(tokenize.decode([1, 17778, 2]))
+print(len(tokenize.encoder))
+print(len(tokenize.decoder))
+get_embedding_matrix()
